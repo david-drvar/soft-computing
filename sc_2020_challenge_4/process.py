642c85285e33f91aee18c4244a706e73a4768fa2
David Drvar <david.drvar.bogdanovic@gmail.com>
2021-01-02 18:44:31 +0100

    #4 - tesseract basic, without lineboxbuilder

--- sc_2020_challenge_4/process.py
+++ sc_2020_challenge_4/process.py
@@ -1,11 +1,23 @@
 # import libraries here
 import datetime
+import cv2
+from PIL import Image
+import sys
+
+import pyocr
+import pyocr.builders
+
+import pytesseract
+
+import matplotlib
+import matplotlib.pyplot as plt
 
 
 class Person:
     """
     Klasa koja opisuje prepoznatu osobu sa slike. Neophodno je prepoznati samo vrednosti koje su opisane u ovoj klasi
     """
+
     def __init__(self, name: str = None, date_of_birth: datetime.date = None, job: str = None, ssn: str = None,
                  company: str = None):
         self.name = name
@@ -28,7 +40,62 @@ def extract_info(models_folder: str, image_path: str) -> Person:
     """
     person = Person('test', datetime.date.today(), 'test', 'test', 'test')
 
+    tools = pyocr.get_available_tools()
+    if len(tools) == 0:
+        print("No OCR tool found")
+        sys.exit(1)
+
+    # odaberemo Tessract - prvi na listi ako je jedini alat
+    tool = tools[0]
+    print("Koristimo backend: %s" % (tool.get_name()))
+    # biramo jezik očekivanog teksta
+    lang = 'eng'
+
     # TODO - Prepoznati sve neophodne vrednosti o osobi sa slike. Vrednosti su: Name, Date of Birth, Job,
     #       Social Security Number, Company Name
+
+    image = cv2.imread(image_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    plt.imshow(image)
+    plt.show()
+
+    text = tool.image_to_string(
+        Image.fromarray(image),
+        lang=lang,
+        builder=pyocr.builders.TextBuilder(tesseract_layout=3)  # izbor segmentacije (PSM)
+    )
+
+
+    # todo izlaz po redovima
+    line_and_word_boxes = tool.image_to_string(
+        Image.fromarray(image), lang=lang,
+        builder=pyocr.builders.LineBoxBuilder(tesseract_layout=3)
+    )
+    for i, line in enumerate(line_and_word_boxes):
+        print('line %d' % i)
+        print(line.content, line.position)
+        print('boxes')
+        for box in line.word_boxes:
+            print(box.content, box.position, box.confidence)
+        print()
+
+    # todo izlaz lista reči sa tekstom, koordinatama i faktorom sigurnosti
+    # word_boxes = tool.image_to_string(
+    #     Image.fromarray(image),
+    #     lang=lang,
+    #     builder=pyocr.builders.WordBoxBuilder(tesseract_layout=3)
+    # )
+    # for i, box in enumerate(word_boxes):
+    #     print("word %d" % i)
+    #     print(box.content, box.position, box.confidence)
+    #     print()
+
+    # todo detekcija broja
+    digits = tool.image_to_string(
+        Image.fromarray(image),
+        lang=lang,
+        builder=pyocr.builders.DigitBuilder(tesseract_layout=3)  # ocekivani text je single line, probati sa 3,4,5..
+    )
 
     return person
